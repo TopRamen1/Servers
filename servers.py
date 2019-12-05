09073fb9af6f515aeeec6e16c39d9d753bd2e0c6
TopRamen1 <49471089+TopRamen1@users.noreply.github.com>
2019-12-05 12:25:53 +0100

    Add files via upload

--- servers.py
+++ servers.py
@@ -1,22 +1,12 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from typing import Optional, List
+from typing import Optional, List, Dict
 from abc import ABC, abstractmethod
+import re
 
 
 class Product:
-    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
     def __init__(self, name_: str, price_: float):
         self.name = name_
         self.price = price_
-        name_char_ = []
-        for char in self.name:
-            if char.isalpha():
-                name_char_.append(char)
-
-        self.name_char = name_char_
-
 
     def __hash__(self):
         return hash((self.name, self.price))
@@ -26,26 +16,17 @@ class Product:
 
 
 class ServerErr(Exception):
-    def __init__(self, msg=None):
-        if msg is None:
-            msg = "Error occured"
-
+    def __init__(self, msg="Server Error"):
         super().__init__(msg)
+
 
 class TooManyProductsFoundError(ServerErr):
-    def __init__(self):
-        msg = "To many products found"
+    def __init__(self, msg="To many products found"):
         super().__init__(msg)
-
-
-
-# FIXME: Każada z poniższych klas serwerów powinna posiadać:
-#   (1) metodę inicjalizacyjną przyjmującą listę obiektów typu `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze,
-#   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
-#   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
 
 
 class Server(ABC):
+    n_max_returned_entries = 3
 
     @abstractmethod
     def get_entries(self, n: int) -> List[Product]:
@@ -55,20 +36,21 @@ class Server(ABC):
     def to_many_products_error(self) -> TooManyProductsFoundError:
         pass
 
+
 class ListServer(Server):
 
-    def __init__(self, products_list_: List[Product], n_max_returned_entries_: int = 1):
-        self.products_list=products_list_[:]
-        self.n_max_returned_entries = n_max_returned_entries_
+    def __init__(self, products_list_: List[Product]):
+        self.products_list = products_list_[:]
 
     def to_many_products_error(self) -> TooManyProductsFoundError:
         raise TooManyProductsFoundError
 
-    def get_entries(self, n: int) -> List[Product]:
+    def get_entries(self, n_letters: int = 1) -> List[Product]:
         result = []
-        for e in self.products_list:
-            if len(e.name_char) == n:
-                result.append(e)
+        for product in self.products_list:
+            search_pattern = re.compile('^[a-zA-Z]{{{n}}}\\d{{2,3}}$'.format(n=n_letters))
+            if re.match(search_pattern, product.name):
+                result.append(product)
 
         if len(result) > self.n_max_returned_entries:
             self.to_many_products_error()
@@ -76,14 +58,42 @@ class ListServer(Server):
         return result
 
 
-class MapServer:
-    pass
+class MapServer(Server):
+
+    def __init__(self, products_list_: List[Product]):
+        self.products_dict = {}
+
+        for product in products_list_:
+            self.products_dict[product.name] = product
+
+    def to_many_products_error(self) -> TooManyProductsFoundError:
+        raise TooManyProductsFoundError
+
+    def get_entries(self, n_letters: int = 1):
+        result = []
+        for name, product in self.products_dict.items():
+            search_pattern = re.compile('^[a-zA-Z]{{{n}}}\\d{{2,3}}$'.format(n=n_letters))
+            if re.match(search_pattern, product.name):
+                result.append(product)
+
+        if len(result) > self.n_max_returned_entries:
+            self.to_many_products_error()
+
+        return result
 
 
 class Client:
-    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
+    def __init__(self, server_: Server):
+        self.server = server_
 
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
-        pass
+        total_price = 0
 
-    pass
+        try:
+            list_of_products = self.server.get_entries(n_letters)
+        except TooManyProductsFoundError:
+            return None
+
+        for elem in list_of_products:
+            total_price = total_price + elem.price
+        return total_price
